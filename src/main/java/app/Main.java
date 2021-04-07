77a93b40209bc92ecbd5e33d150f4668edb7df96
Lukasz <lukasz.lis@ext.ec.europa.eu>
2021-04-07 16:13:13 +0200

    Initial commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,21 @@ package app;
 
 public class Main {
 
+    /**
+     * Aby uruchomić projekt musisz przejść w Intellij: Open --> znaleźć ściężkę do projektu --> zaznaczyć folder pass-braker.
+     * W prawym dolnym ekranie pojawi się popup z informacją o "eneble auto import" --> kliknij na to.
+     * Jeśli nie wyskoczy ten popup, to: prawy klawisz myszy na plik pom.xml --> Maven --> Reimport.
+     * W klasie Main poniżej możesz mieć na czerwono RandomStringUtils. Kliknij na tym i wciśnij Alt + Enter i zaznacz import.
+     * Nie masz import? to powtórz trzecią gwiazdkę :)
+     * Uruchom zielonym buttonem.
+     * ENJOY!
+     * */
+
     public static void main(String[] args) {
 
-
-        System.out.println("Hello twoja stara");
+        PassBreaker passwordToHack = new PassBreaker();
+        passwordToHack.passBreaker("Twoja");
+        //System.out.println("Hello twoja stara");
 
     }
 
--- src/main/java/app/PassBreaker.java
+++ src/main/java/app/PassBreaker.java
@@ -1,4 +1,38 @@
 package app;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 public class PassBreaker {
+
+    public void passBreaker(String password) {
+        String pass = null;
+        //boolean flag = true;
+        BigInteger counter = BigInteger.ZERO;
+        long start = System.nanoTime();
+
+        while (true) {
+            pass = RandomStringUtils.randomAlphabetic(password.length());
+            counter = counter.add(BigInteger.ONE);
+            if (pass.equals(password)) {
+                break;
+            }
+//            System.out.println(pass + " | " + counter);
+//
+//            try {
+//                Thread.sleep(200);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+        }
+
+        long stop = System.nanoTime();
+        double elapsedTime = (double) (stop - start) / 1_000_000_000;
+        double convert = TimeUnit.MILLISECONDS.convert((stop - start), TimeUnit.NANOSECONDS);
+        System.out.println("BROKEN! " + convert + " | " + pass + " at time sec: " + elapsedTime + " --> Number of tries: " + counter);
+    }
+
 }
